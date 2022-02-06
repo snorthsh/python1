5c86ac8eb7350971b681fc6259465a766f423ae2
snorthsh <58538749+snorthsh@users.noreply.github.com>
2022-02-05 22:47:22 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -24,14 +24,19 @@ def welcome_assignment_answers(question):
     elif question ==  "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "Yes"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = "1"
+        answer = "5"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "Yes"
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = "2"
+        answer = "4"
     return(answer)
 # Complete all the questions.
 
+
+if __name__ == "__main__":
+    #use this space to debug and verify that the program works
+    debug_question = "Are encoding and encryption the same? - Yes/No"
+    print(welcome_assignment_answers(debug_question))
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
